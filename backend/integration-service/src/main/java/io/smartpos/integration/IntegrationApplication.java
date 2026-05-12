a89813f13330f3e6cc9be55378ae258fa00abe91
Ismael mkumbi <ismaailimkumbi@gmail.com>
2026-05-12 03:45:02 +0300

    feat: enable JPA auditing in all backend services

--- backend/integration-service/src/main/java/io/smartpos/integration/IntegrationApplication.java
+++ backend/integration-service/src/main/java/io/smartpos/integration/IntegrationApplication.java
@@ -1,12 +1,23 @@
 package io.smartpos.integration;
 
+import io.smartpos.common.audit.JwtAuditorAware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class IntegrationApplication {
+
+    @Bean
+    public AuditorAware<String> auditorAware() {
+        return new JwtAuditorAware();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(IntegrationApplication.class, args);
     }
