47ec54f7f1b3e121a2eb0ac9b931209028a1a90e
Ismael mkumbi <ismaailimkumbi@gmail.com>
2026-05-30 12:48:13 +0300

    fix(brand): add @EnableCaching to SalesApplication so brand cache works

--- backend/sales-service/src/main/java/io/smartpos/sales/SalesApplication.java
+++ backend/sales-service/src/main/java/io/smartpos/sales/SalesApplication.java
@@ -3,6 +3,7 @@ package io.smartpos.sales;
 import io.smartpos.common.audit.JwtAuditorAware;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 @EnableFeignClients(basePackages = "io.smartpos.sales.infrastructure.feign")
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class SalesApplication {
